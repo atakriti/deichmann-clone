45a722a02150c39b4a3d77d309f3c5a7d109e152
atakriti <anwart256@gmail.com>
2022-04-03 15:27:50 +0200

    app done

---
+++ src/javaScript/main.js
@@ -0,0 +1,28 @@
+function btn() {
+  let list = document.getElementById("list");
+  if (list.style.display === "none") {
+    list.style.display = "block";
+    list.style.animationName = "listDown";
+  } else {
+    
+     list.style.display = "none";
+  }
+}
+
+function sectionBtn() {
+  let list = document.getElementById("list");
+  if (list.style.display === "block") {
+    list.style.display = "none";
+  } else {
+    list.style.display = "block";
+  }
+}
+
+function main() {
+  let list = document.getElementById("list");
+  if (list.style.display === "block") {
+    list.style.display = "none";
+  } else {
+    list.style.display = "none";
+  }
+}
